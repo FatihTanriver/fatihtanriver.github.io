9a548ec1933f6fddf4e8da0d36d8a1d2bc37ec6a
Fatih <fatihtanriver@gmail.com>
2023-10-24 13:08:14 +0200

    Add functionality for switcher

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+  let langKey = localStorage.getItem("language");
+  if (langKey == null) {
+    changeLanguage("de");
+  } else {
+    changeLanguage(langKey);
+  }
+
+  let attr = $("body").attr("data-theme");
+
+  // For some browsers, `attr` is undefined; for others,
+  // `attr` is false.  Check for both.
+  if (typeof attr !== "undefined" && attr !== false) {
+    $("#switcher-moon").css("fill", "white");
+  } else {
+    $("#switcher-sun").css("fill", "yellow");
+  }
+
   $(window).scroll(function () {
     if ($(this).scrollTop() > 0) {
       $("header").addClass("shrink");
@@ -7,11 +24,17 @@ $(document).ready(function () {
     }
   });
 
-  $("#theme-toggle-btn").click(function () {
-    if ($("body").attr("data-theme") === "night") {
-      $("body").removeAttr("data-theme");
-    } else {
+  $("#theme-toggle-checkbox").change(function () {
+    var switcherIndicator = $(".switcher__indicator::after");
+
+    if ($(this).is(":checked")) {
       $("body").attr("data-theme", "night");
+      $("#switcher-sun").css("fill", "none");
+      $("#switcher-moon").css("fill", "white");
+    } else {
+      $("body").removeAttr("data-theme");
+      $("#switcher-sun").css("fill", "yellow");
+      $("#switcher-moon").css("fill", "none");
     }
   });
 
@@ -41,14 +64,34 @@ $(document).ready(function () {
     }
   });
 
+  let timeout;
+  let isTimeoutActive = false;
   $(".navRad .mask").on("touchstart click", function (e) {
     e.preventDefault();
     var $parent = $(this).parent();
     $parent.toggleClass("active");
 
-    // Close the parent element with a transition after three seconds
-    setTimeout(function () {
+    // Clear the existing timeout if it's active
+    if (isTimeoutActive) {
+      clearTimeout(timeout);
+      isTimeoutActive = false;
+    }
+
+    // Start a new timeout to close the parent element after three seconds
+    timeout = setTimeout(function () {
       $parent.removeClass("active");
+      isTimeoutActive = false;
+    }, 3000);
+    isTimeoutActive = true;
+  });
+
+  // Restart the timeout if you click on any .nav-item
+  $(".navRad-item").on("touchstart click", function () {
+    clearTimeout(timeout);
+    isTimeoutActive = false;
+    timeout = setTimeout(function () {
+      // Code to remove "active" class from parent here
+      $(".navRad.active").removeClass("active");
     }, 3000);
   });
 
@@ -58,6 +101,86 @@ $(document).ready(function () {
     $(".html").removeClass("visible");
     $(".html." + t).addClass("visible");
     $(".navRad.left-bottom").toggleClass("active");
-    App.title($(this).text());
+    clearTimeout(timeout);
   });
 });
+
+/*Start Language Translation*/
+function changeLanguage(langKey) {
+  //localStorage.getItem("language") == null ? setLanguage(langKey) : false;
+  setLanguage(langKey);
+  language = localStorage.getItem("language");
+  var jsonUrl = "";
+
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    //'http://localhost:5500/language/de.json'
+    jsonUrl =
+      window.location.origin +
+      "/language/" +
+      localStorage.getItem("language") +
+      ".json";
+  } else {
+    jsonUrl =
+      window.location.origin +
+      window.location.pathname +
+      "language/" +
+      localStorage.getItem("language") +
+      ".json";
+  }
+
+  $.ajax({
+    url: jsonUrl,
+    type: "HEAD",
+    error: function () {
+      console.log("The file does not exist.");
+      jsonUrl =
+        window.location.origin +
+        window.location.pathname +
+        "language/" +
+        localStorage.getItem("language") +
+        ".json";
+    },
+    success: function () {
+      // console.log("The file exists.");
+    },
+  });
+
+  $.ajax({
+    url: jsonUrl,
+    dataType: "json",
+    async: false,
+    dataType: "json",
+    success: function (lang) {
+      language = lang;
+    },
+  });
+
+  updateTranslations();
+
+  $(".site-language img").css("opacity", "0.5");
+  $(`.icon-${langKey} img`).css("opacity", "1");
+}
+
+function setLanguage(lang) {
+  localStorage.setItem("language", lang);
+}
+
+function updateTranslations() {
+  //language = localStorage.getItem("language");
+  var newVal = language.date;
+  $("[data-lang=navigation_text_1]").text(language.navigation_text_1);
+  $("[data-lang=navigation_text_2]").text(language.navigation_text_2);
+  $("[data-lang=navigation_text_3]").text(language.navigation_text_3);
+  $("[data-lang=navigation_text_4]").text(language.navigation_text_4);
+  $("[data-lang=navigation_text_5]").text(language.navigation_text_5);
+  $("[data-lang=hero_text_1]").text(language.hero_text_1);
+}
+/*End Language Translation*/
+
+/*Start Color Palette*/
+
+function changePalette(newClass) {
+  $("body").removeClass("palette-1 palette-2 palette-3").addClass(newClass);
+}
+
+/*End Color Palette */
